51433114ce5d17a539173fb918999a9989772a3f
miles-hollifield <60891981+miles-hollifield@users.noreply.github.com>
2022-01-18 18:41:31 -0500

    Finishing touches to header.php and footer.php

--- includes/header.php
+++ includes/header.php
@@ -23,37 +23,37 @@ include_once 'includes/session.php';
 </head>
 
 <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php">IT Conference</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mr-auto">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">IT Conference</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse container" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="viewrecords.php">View Attendees</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <?php if (!isset($_SESSION['userid'])) { ?>
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            <a class="nav-item nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item">
+                            <a class="nav-item nav-link" href="#"><span>Hello <?php echo $_SESSION['username'] ?>! </span> <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="viewrecords.php">View Attendees</a>
+                            <a class="nav-item nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav ml-auto">
-                        <?php if (!isset($_SESSION['userid'])) { ?>
-                            <li class="nav-item">
-                                <a class="nav-item nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
-                            </li>
-                        <?php } else { ?>
-                            <li class="nav-item">
-                                <a class="nav-item nav-link" href="#"><span>Hello <?php echo $_SESSION['username'] ?>! </span> <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-item nav-link" href="logout.php">Logout <span class="sr-only">(current)</span></a>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                </div>
+                    <?php } ?>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
+    <div class="container">
         <br>
